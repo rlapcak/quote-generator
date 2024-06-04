fa4329b362fafc7a75ada6f8aaa83efd7483a326
Róbert Lapčák <160675740+rlapcak@users.noreply.github.com>
2024-06-04 22:42:57 +0200

    Update script.js
    rename loader functions

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ const loader = document.getElementById('loader');
 let apiQuotes = [];
 
 //Show Loading
-function loading(){
+function showLoadingSpinner(){
     loader.hiddnen = false;
     quoteContainer.hidden = true;
 }
 
 // Hide Loading
-function complete(){
+function removeLoadingSpinner(){
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
 
 //Show New Quote
 function newQuote() {
-    loading();
+    showLoadingSpinner();
     // Pick a random Quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank and replace it with "Unknown"
@@ -38,12 +38,12 @@ function newQuote() {
     }
     // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpinner();
 }
 
 // Get Quotes From API
 async function getQuotes () {
-    loading();
+    showLoadingSpinner();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
